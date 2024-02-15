5e83dd65fa96824022f4b8c2475861bffd2c0b6a
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-02-15 18:18:38 -0300

    ctlr, rpstry, srvc, mdl

---
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/controller/ProdutoController.java
@@ -0,0 +1,24 @@
+package com.teste.primeiroexemplo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.primeiroexemplo.model.Produto;
+import com.teste.primeiroexemplo.services.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+    
+    @GetMapping
+    public List <Produto> obterTodos(){
+        return produtoService.obterTodos();
+    }
+}
