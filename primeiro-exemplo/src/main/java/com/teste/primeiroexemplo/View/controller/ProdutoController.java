043487b096a291b1484b7e69041b29afe55c1132
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-02-23 15:06:28 -0300

    Pequenas correções

--- primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/View/controller/ProdutoController.java
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/View/controller/ProdutoController.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.teste.primeiroexemplo.View.model.ProdutoRequest;
 import com.teste.primeiroexemplo.View.model.ProdutoResponse;
-import com.teste.primeiroexemplo.model.Produto;
 import com.teste.primeiroexemplo.services.ProdutoService;
 import com.teste.primeiroexemplo.shared.ProdutoDTO;
 
